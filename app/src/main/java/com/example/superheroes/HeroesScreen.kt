989ce5f665924b2fc5165aaf75704b8d75637e35
David Corcuera <david.corcuera@gmail.com>
2023-08-31 19:07:59 +0200

    add TopAppBar

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -14,7 +14,10 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,21 +28,27 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.superheroes.model.Hero
-import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.model.HeroesRepository.heroes
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HeroesList(
     heroes: List<Hero>,
     modifier: Modifier = Modifier,
 ) {
-    LazyColumn() {
-        items(heroes) { hero ->
-            HeroItem(
-                hero,
-                modifier.padding(horizontal = 16.dp, vertical = 8.dp)
-            )
+    Scaffold(
+        topBar = {
+            SuperheroesTopAppBar()
+        }
+    ) { it ->
+        LazyColumn(contentPadding = it) {
+            items(heroes) { hero ->
+                HeroItem(
+                    hero,
+                    modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                )
+            }
         }
     }
 }
@@ -83,6 +92,20 @@ fun HeroItem(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SuperheroesTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        modifier = modifier,
+        title = {
+            Text(
+                text = stringResource(id = R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+        }
+    )
 }
 
 @Preview
